51cfd08854981e3ebf0bc61ffd463f9bc2f41f04
Mohammad Islam <saikat_mist@yahoo.com>
2022-11-29 13:27:56 -0800

    add bank page cards and input fields

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ document.getElementById("login-btn").addEventListener('click',function(){
    const password = document.getElementById("pwd").value;
    //Never do password and email validation on client side. This is just for testing purporse
    if(userId === "saikat2021@gmail.com" && password === "test1234") {
-    alert("Login successful");
+    window.location.href="./bank.html";
    }
    else {
     alert("Please enter correct email id and password")
