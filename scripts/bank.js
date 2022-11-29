89d398115ad00b870ce82d9b412b94d28f75fee2
Mohammad Islam <saikat_mist@yahoo.com>
2022-11-29 14:36:23 -0800

    change code format and bug fixes

--- scripts/bank.js
+++ scripts/bank.js
@@ -1,15 +1,25 @@
-document.getElementById("deposit-btn").addEventListener("click",function(){
-    var depositAmount = document.getElementById("deposit-amnt").value;
-    var totalDepositAmount = document.getElementById("deposit-current-amnt").innerText;
-    var balanceAmount =  document.getElementById("balance-amnt").innerText;
-    document.getElementById("deposit-current-amnt").innerText = parseFloat(depositAmount) + parseFloat(totalDepositAmount);
-    document.getElementById("balance-amnt").innerText  =  parseFloat(balanceAmount) + parseFloat(depositAmount) ;
+document.getElementById("deposit-btn").addEventListener("click", function () {
+  var depositAmount = document.getElementById("deposit-amnt").value;
+  var totalDepositAmount = document.getElementById(
+    "deposit-current-amnt"
+  ).innerText;
+  var balanceAmount = document.getElementById("balance-amnt").innerText;
+  document.getElementById("deposit-current-amnt").innerText =
+    parseFloat(depositAmount) + parseFloat(totalDepositAmount);
+  document.getElementById("balance-amnt").innerText =
+    parseFloat(balanceAmount) + parseFloat(depositAmount);
+  document.getElementById("deposit-amnt").value = "";
 });
 
-document.getElementById("withdraw-btn").addEventListener("click",function(){
-    var withdrawAmount = document.getElementById("withdraw-amnt").value;
-    var totalWithdrawAmount = document.getElementById("withdraw-current-amnt").innerText;
-    var balanceAmount = document.getElementById("balance-amnt").innerText;
-    document.getElementById("withdraw-current-amnt").innerText = parseFloat(withdrawAmount) + parseFloat(totalWithdrawAmount);
-    document.getElementById("balance-amnt").innerText = parseFloat(balanceAmount) - parseFloat(withdrawAmount);
+document.getElementById("withdraw-btn").addEventListener("click", function () {
+  var withdrawAmount = document.getElementById("withdraw-amnt").value;
+  var totalWithdrawAmount = document.getElementById(
+    "withdraw-current-amnt"
+  ).innerText;
+  var balanceAmount = document.getElementById("balance-amnt").innerText;
+  document.getElementById("withdraw-current-amnt").innerText =
+    parseFloat(withdrawAmount) + parseFloat(totalWithdrawAmount);
+  document.getElementById("balance-amnt").innerText =
+    parseFloat(balanceAmount) - parseFloat(withdrawAmount);
+  document.getElementById("withdraw-amnt").value = "";
 });
--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,10 @@
-document.getElementById("login-btn").addEventListener('click',function(){
-   const userId = document.getElementById("username").value;
-   const password = document.getElementById("pwd").value;
-   //Never do password and email validation on client side. This is just for testing purporse
-   if(userId === "saikat2021@gmail.com" && password === "test1234") {
-    window.location.href="./bank.html";
-   }
-   else {
-    alert("Please enter correct email id and password")
-   }
-})
+document.getElementById("login-btn").addEventListener("click", function () {
+  const userId = document.getElementById("username").value;
+  const password = document.getElementById("pwd").value;
+  //Never do password and email validation on client side. This is just for testing purporse
+  if (userId === "saikat2021@gmail.com" && password === "test1234") {
+    window.location.href = "./bank.html";
+  } else {
+    alert("Please enter correct email id and password");
+  }
+});
