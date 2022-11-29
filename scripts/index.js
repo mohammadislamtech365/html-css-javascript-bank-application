512bd646016d3cbbbc628150e23c7abdbe39caef
Mohammad Islam <saikat_mist@yahoo.com>
2022-11-29 12:37:26 -0800

    add event handler in login button

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+document.getElementById("login-btn").addEventListener('click',function(){
+   const userId = document.getElementById("username").value;
+   const password = document.getElementById("pwd").value;
+   //Never do password and email validation on client side. This is just for testing purporse
+   if(userId === "saikat2021@gmail.com" && password === "test1234") {
+    alert("Login successful");
+   }
+   else {
+    alert("Please enter correct email id and password")
+   }
+})
